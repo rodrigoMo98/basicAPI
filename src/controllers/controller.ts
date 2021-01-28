70a20ab5a2a54136fc309603c08064a2d044916e
Rodrigo Mollinedo <Rodrigo.Mollinedo@jala-foundation.org>
2021-01-28 17:22:36 -0400

    fixed duplication data file

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,28 +1,25 @@
 import { Request, Response } from "express";
-import * as data from '../data/data.json';
 
-export async function get(req : Request, res:Response): Promise<Response<any, Record<string, any>>>{
+export function get(req : Request, res:Response): Response{
     const fs = require('fs');
     let content2 = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
     return  res.json('Data list: '+JSON.stringify(content2));
 }
 
-export async function add(req : Request, res:Response) {
+export function add(req : Request, res:Response): Response{
     //reciebe json from request
     const newReq = req.body;
     //manage data.json
     const fs = require('fs');
-    let content = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
+    let content = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
     //add new data to json
     const returnedTarget = Object.assign(content, newReq);
-    await fs.writeFileSync('src/data/data.json ', JSON.stringify(returnedTarget));
-    //read data.json and response
-    let content2 = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
-    return  res.json('Data added: '+JSON.stringify(content2));
+    fs.writeFileSync('src/data/data.json', JSON.stringify(returnedTarget));
+    return  res.json('Data added: '+JSON.stringify(returnedTarget));
 
 }
 
-export async function del(req : Request, res:Response) {
+export function del(req : Request, res:Response): Response {
     //reciebe json from request
     const newReq = req.body;
     const fs = require('fs');
@@ -30,8 +27,6 @@ export async function del(req : Request, res:Response) {
     let content = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
     //delete data from json
     delete content[newReq.id];
-    await fs.writeFileSync('src/data/data.json', JSON.stringify(content));
-    //read data.json and response
-    let content2 = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
-    return  res.json('Data deleted: '+JSON.stringify(content2));
+    fs.writeFileSync('src/data/data.json', JSON.stringify(content));
+    return  res.json('Data deleted: '+JSON.stringify(content));
 }
