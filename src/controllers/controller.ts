f28b5bb1fb3b609492f514725c540d9fb301854f
Rodrigo Mollinedo <Rodrigo.Mollinedo@jala-foundation.org>
2021-01-28 18:34:41 -0400

    final changes adding list all

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express";
 
 export function get(req : Request, res:Response): Response{
+    //recibe params and read data.json file
     const newReq = req.params;
-    console.log('PARAM: '+newReq.id);
     const fs = require('fs');
     let content2 = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
-    if(content2["Nombre " +newReq.id] == undefined)
-        return  res.json('HOLA MUNDO!! No encontramos tu nombre :(');
-    return  res.json('Hola '+content2["Nombre " +newReq.id]+'!!');
+    let vari :string=newReq.id;
+    //decide wich message return
+    if(vari == '-1')
+        return  res.json(content2);
+    if(content2[vari] == undefined)
+       return  res.json('HOLA MUNDO!! No encontramos tu nombre :(');
+    return  res.json('Hola '+content2[newReq.id]+'!!');
 }
 
 export function add(req : Request, res:Response): Response{
@@ -16,33 +20,35 @@ export function add(req : Request, res:Response): Response{
     //manage data.json
     const fs = require('fs');
     let content = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
-    //add new data to 
+    //add new data to json
     content.cont++;
-    content["Nombre " +content.cont]=newReq.nombre;
+    content[content.cont]=newReq.nombre;
     fs.writeFileSync('src/data/data.json', JSON.stringify(content));
-    return  res.json('Nombre agregado! Hola '+ content["Nombre " +content.cont]+'!!');
+    return  res.json('Nombre agregado! Hola '+ content[content.cont]+'!! Tu id es: '+ content.cont);
 }
 
 export function edit(req : Request, res:Response): Response{
     //reciebe json from request
+    const reqParam = req.params;
+    let vari :string=reqParam.id
     const newReq = req.body;
     //manage data.json
     const fs = require('fs');
     let content = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
-    //add new data to 
-    content["Nombre " +newReq.id]=newReq.nombre;
+    //edit new data
+    content[vari]=newReq.nombre;
     fs.writeFileSync('src/data/data.json', JSON.stringify(content));
-    return  res.json('Nombre editado! Hola de nuevo '+ content["Nombre " +newReq.id]+'!!');
+    return  res.json('Nombre editado! Hola de nuevo '+ content[vari]+'!!');
 }
 
 export function del(req : Request, res:Response): Response {
     //reciebe json from request
-    const newReq = req.body;
+    const newReq:string = req.params.id;
     const fs = require('fs');
     //manage data.json
     let content = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
     //delete data from json
-    delete content["Nombre "+newReq.id];
+    delete content[newReq];
     fs.writeFileSync('src/data/data.json', JSON.stringify(content));
-    return  res.json('Data deleted: '+JSON.stringify(content));
+    return  res.json('Nombre eliminado, adios :(');
 }
