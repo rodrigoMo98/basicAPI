5d0b7a450abf7798a923ad5ea26224b9a60fc663
Rodrigo Mollinedo <Rodrigo.Mollinedo@jala-foundation.org>
2021-02-02 11:09:32 -0400

    webpack configuration

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -4,7 +4,7 @@ export function get(req : Request, res:Response): Response{
     //recibe params and read data.json file
     const newReq = req.params;
     const fs = require('fs');
-    let content2 = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
+    let content2 = JSON.parse(fs.readFileSync( 'data.json', 'utf8'));
     let vari :string=newReq.id;
     //decide wich message return
     if(vari == '-1')
@@ -19,11 +19,11 @@ export function add(req : Request, res:Response): Response{
     const newReq = req.body;
     //manage data.json
     const fs = require('fs');
-    let content = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
+    let content = JSON.parse(fs.readFileSync('data.json', 'utf8'));
     //add new data to json
     content.cont++;
     content[content.cont]=newReq.nombre;
-    fs.writeFileSync('src/data/data.json', JSON.stringify(content));
+    fs.writeFileSync('data.json', JSON.stringify(content));
     return  res.json('Nombre agregado! Hola '+ content[content.cont]+'!! Tu id es: '+ content.cont);
 }
 
@@ -34,10 +34,10 @@ export function edit(req : Request, res:Response): Response{
     const newReq = req.body;
     //manage data.json
     const fs = require('fs');
-    let content = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
+    let content = JSON.parse(fs.readFileSync('data.json', 'utf8'));
     //edit new data
     content[vari]=newReq.nombre;
-    fs.writeFileSync('src/data/data.json', JSON.stringify(content));
+    fs.writeFileSync('data.json', JSON.stringify(content));
     return  res.json('Nombre editado! Hola de nuevo '+ content[vari]+'!!');
 }
 
@@ -46,9 +46,9 @@ export function del(req : Request, res:Response): Response {
     const newReq:string = req.params.id;
     const fs = require('fs');
     //manage data.json
-    let content = JSON.parse(fs.readFileSync( 'src/data/data.json', 'utf8'));
+    let content = JSON.parse(fs.readFileSync( 'data.json', 'utf8'));
     //delete data from json
     delete content[newReq];
-    fs.writeFileSync('src/data/data.json', JSON.stringify(content));
+    fs.writeFileSync('data.json', JSON.stringify(content));
     return  res.json('Nombre eliminado, adios :(');
 }
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const copyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const ZipPlugin = require('zip-webpack-plugin');
+
+const {
+	NODE_ENV = 'production',
+} = process.env;
+
+
+module.exports = {
+    entry: "./src/index.ts",
+    externals: [nodeExternals()],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "build/bundle.js",
+    },
+    mode: NODE_ENV,
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    target: "node",
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: ["ts-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new copyWebpackPlugin({
+        patterns: [
+          {
+            from: "./package.json",
+            to: "./",
+          }
+        ],
+      }),
+      new ZipPlugin({
+        filename: "bundle.zip",
+      }),
+    ],
+  };
+  
